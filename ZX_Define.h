507b6e33cc2fbccc647774e2292cb9f6c7443d48
zhengxin <zx19880528@qq.com>
2015-01-04 09:07:23 +0800

    modified:   ZX_Define.h 	modified:   category/CGRect+helper.h

--- ZX_Define.h
+++ ZX_Define.h
@@ -10,7 +10,7 @@
 #define APP_DELEGATE ((AppDelegate *)[[UIApplication sharedApplication]delegate])
 
 #define WINDOW_HEIGHT ([[(AppDelegate *)[[UIApplication sharedApplication]delegate] window] height] + (IOS_7?0:-20))
-
+#define WINDOW_WIDTH    [UIScreen mainScreen].bounds.size.width
 //    #define WINDOW_HEIGHT [[UIScreen mainScreen] applicationFrame].size.height
 
 /**
--- category/CGRect+helper.h
+++ category/CGRect+helper.h
@@ -6,18 +6,18 @@
 //  Copyright (c) 2014 luo hao. All rights reserved.
 //
 
-static __inline__ CGRect CGRectFromSize( CGSize size ) {
+CG_INLINE CGRect CGRectFromSize( CGSize size ) {
     return CGRectMake( 0, 0, size.width, size.height );
 };
 
-static __inline__ CGRect CGRectMakeWithCenterAndSize( CGPoint center, CGSize size ) {
+CG_INLINE CGRect CGRectMakeWithCenterAndSize( CGPoint center, CGSize size ) {
     return CGRectMake( center.x - size.width * 0.5, center.y - size.height * 0.5, size.width, size.height );
 };
 
-static __inline__ CGRect CGRectMakeWithOriginAndSize( CGPoint origin, CGSize size ) {
+CG_INLINE CGRect CGRectMakeWithOriginAndSize( CGPoint origin, CGSize size ) {
     return CGRectMake( origin.x, origin.y, size.width, size.height );
 };
 
-static __inline__ CGPoint CGRectCenter( CGRect rect ) {
+CG_INLINE CGPoint CGRectCenter( CGRect rect ) {
     return CGPointMake( CGRectGetMidX( rect ), CGRectGetMidY( rect ) );
 };
