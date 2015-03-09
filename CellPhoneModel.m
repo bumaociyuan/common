88f6b1faa843048754e1bdf6e456a4b4416c52b8
zhengxin <zx19880528@qq.com>
2015-03-09 17:38:18 +0800

    add iphone6 in cellphonemodel.m
    	modified:   CellPhoneModel.m

--- CellPhoneModel.m
+++ CellPhoneModel.m
@@ -44,6 +44,10 @@ NSString * machineName() {
     
     if ([platform isEqualToString:@"iPhone6,2"]) return @"iPhone 5s (GSM+CDMA)";
     
+    if ([platform isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
+    
+    if ([platform isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
+    
     if ([platform isEqualToString:@"iPod1,1"]) return @"iPod Touch 1G";
     
     if ([platform isEqualToString:@"iPod2,1"]) return @"iPod Touch 2G";
